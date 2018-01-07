d76cbce92276c56a8eed138446252d83ecdf38fc
ChristianBZG <35174222+ChristianBZG@users.noreply.github.com>
2018-01-07 11:47:20 +0100

    Ajout d'un fichier .INO
    Ce fichier est le programme en C pour la carte Arduino Uno

---
+++ Va_et_vient.ino
@@ -0,0 +1,19 @@
+// Programme de va et vient pour train miniature
+// Le signal est sur la sortie broche
+byte broche = 4 ;
+unsigned int T_periode ;
+int Val_pot ;
+void setup ()
+{
+  pinMode (broche, OUTPUT) ;
+}
+void loop ()
+{
+  Val_pot = analogRead (0) ;
+  T_periode = map(Val_pot, 0, 1023, 25000, 50000) ;  // mini 25 s maxi 50 s
+  digitalWrite (broche, HIGH) ;
+  delay (T_periode) ;
+  digitalWrite (broche, LOW) ;
+  delay (T_periode) ;
+}
+
